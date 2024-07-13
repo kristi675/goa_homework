99ecb2289531a78bf378d2022144f0e7897de60f
kristi675 <mchedlidzeaniran2@gmail.com>
2024-07-13 14:40:13 +0400

    second commit

---
+++ level 09/homework/lesson1.py
@@ -0,0 +1,27 @@
+# num = int(input("please enter your num: "))
+# num1 = int(input("please enter your num: "))
+# print(num + num1)
+# print(num - num1)
+# print(num1 // num)
+# print(num * num1)
+# print(num1 % num)
+
+# num = int(input("please enter your num: "))
+# num1 = int(input("please enter your num1: "))
+# print(num + num1)
+# print(num1 // num)
+# print(num * num1)
+# print(num1 - num)
+# print(num1 % num)
+
+
+# age = int(input("please enter your age: "))
+# print(age + 10)
+
+
+# num = int
+# num1 = float
+# num2 = str
+# print(num)
+# print(num1)
+# print(num2)
