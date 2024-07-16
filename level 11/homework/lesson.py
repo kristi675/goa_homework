3f645545fa8157baf99d5a87a1bb6c88ef62d0b2
kristi675 <mchedlidzeaniran2@gmail.com>
2024-07-16 18:30:20 +0400

    second commit

---
+++ level 11/homework/lesson.py
@@ -0,0 +1,7 @@
+# mail = str(input("please enter your mail: "))
+# password = str(input("please enter your password: "))
+# print(password == mail)
+
+age = int(input("please enter your age: "))
+my_age = 11
+print(age == my_age)
