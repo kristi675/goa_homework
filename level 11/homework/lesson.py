a2537c327fb87ea0c6693c1b5d785b5c97bae57b
kristi675 <mchedlidzeaniran2@gmail.com>
2024-07-20 20:41:30 +0400

    second commit

--- level 11/homework/lesson.py
+++ level 11/homework/lesson.py
@@ -1,7 +1,12 @@
-# mail = str(input("please enter your mail: "))
-# password = str(input("please enter your password: "))
-# print(password == mail)
+mail = input("please enter your mail: ")
+password =input("please enter your password: ")
+email_2 = input("please reenter your email: ")
+password_2 =input("please reenter your password: ")
+print(mail == email_2 and password == password_2)
 
-age = int(input("please enter your age: "))
-my_age = 11
-print(age == my_age)
+
+
+# age_1 = int(input("please enter your age: "))
+# age_2 = int(input("please enter your age: "))
+# my_age = 11
+# print(age_1 == my_age or age_2 == my_age )
