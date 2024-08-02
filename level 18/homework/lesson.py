4281ccf474d1ea9c2da2a285d494994c3be1998e
kristi675 <mchedlidzeaniran2@gmail.com>
2024-08-02 21:12:32 +0400

    second commit

---
+++ level 18/homework/lesson.py
@@ -0,0 +1,77 @@
+# for i in range(0,50 + 1,4):
+#     print(i)
+
+# for i in range(1,100 + 1,2):
+#     print(i)
+
+# for i in range(2,10 + 1,2):
+#     print(i)
+
+# for i in range(0,15,2):
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(0,12 + 1,2):
+#     print(i)
+
+# for i in range(0,34 + 1,2):
+#     print(i)
+
+# for i in range(0,63,1):
+#     print(i)
+
+# for i in range(0,20 + 1,2):
+#     print(i)
+
+# for i in range(2,40 + 1,2):
+#     print(i)
+
+# for i in range(1,60 + 1,2):
+#     print(i)
+
+
+num = int(input("please enter your num: "))
+while num != 11:
+    num=int(input("please enter your num: "))
+    print("you guessed the num: ")
+
+num1 = int(input("please enter your num1: "))
+while num1 != 23:
+    num1=int(input("please enter your num1: "))
+
+num2 = int(input("please enter your num2: "))
+while num2 != 100:
+    num2=int(input("please enter your num2: ")) 
+
+num3 = int(input("please enter your num3: "))
+while num3 != 160:
+    num3=int(input("please enter your num3: ")) 
+
+num4 = int(input("please enter your num4: "))
+while num4 != 120:
+    num4=int(input("please enter your num4: ")) 
+
+num5 = int(input("please enter your num5: "))
+while num5 != 10:
+    num5=int(input("please enter your num5: "))
+
+num6 = int(input("please enter your num6: "))
+while num6 != 30:
+    num6=int(input("please enter your num6: "))
+
+num7 = int(input("please enter your num7: "))
+while num7 != 17:
+    num7=int(input("please enter your num7: "))
+
+num8 = int(input("please enter your num8: "))
+while num8 != 47:
+    num8=int(input("please enter your num8: "))
+
+num9 = int(input("please enter your num9: "))
+while num9 != 34:
+    num9=int(input("please enter your num9: "))
+
+
+
