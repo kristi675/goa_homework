3bf8d7c68c8c2b8863cbd9fca372904e808ce17e
kristi675 <mchedlidzeaniran2@gmail.com>
2024-11-08 14:26:06 +0400

    second commit

---
+++ level 45/homework/index.py
@@ -0,0 +1,50 @@
+# def count_items(item_list, item):
+#     return item_list.count(item)
+
+# print(count_items([1, 2, 3, 1, 4, 1], 1)) 
+# print(count_items(['apple', 'banana', 'apple'], 'apple'))  
+# print(count_items(['a', 'b', 'c'], 'd'))
+
+
+# def sum_of_list(num_list):
+#     total = 0  
+#     for num in num_list:  
+#         total += num  
+#     return total
+
+# print(sum_of_list([1, 2, 3, 4, 5])) 
+# print(sum_of_list([-1, -2, -3]))  
+# print(sum_of_list([10, 20, 30]))
+
+
+# def average_of_list(num_list):
+#     if not num_list:  
+#         return 0  
+    
+#     total = 0 
+#     for num in num_list: 
+#         total += num  
+    
+#     average = total / len(num_list)
+#     return average  
+
+#     print(average_of_list([1, 2, 3, 4, 5]))  
+# print(average_of_list([-1, 0, 1]))  
+# print(average_of_list([10, 20, 30]))  
+# print(average_of_list([]))
+
+
+
+# def reverse_list(items):
+#     reversed_list = []  
+#     for item in items:  
+#         reversed_list.insert(0, item)  
+#     return reversed_list
+
+# def reverse_list(items):
+#     return items[::-1]
+
+#     print(reverse_list([1, 2, 3, 4, 5]))  
+# print(reverse_list(['apple', 'banana', 'cherry']))  
+# print(reverse_list([]))
+
