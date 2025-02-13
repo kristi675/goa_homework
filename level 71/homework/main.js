0510d353bb21504d82e9b7adabffd5e4d3279644
kristi675 <mchedlidzeaniran2@gmail.com>
2025-02-13 22:25:21 +0400

    second commit

---
+++ level 71/homework/main.js
@@ -0,0 +1,51 @@
+// const car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2022,
+//     color: "Black",
+//     weight: "1500kg"
+// };
+
+// console.log(car);
+
+
+
+// const car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2022,
+//     color: "Black",
+//     weight: "1500kg",
+    
+//     getDescription: function() {
+//         return `ამ მანქანის ბრენდია ${this.brand}, კონკრეტული მოდელია ${this.model}, გამოშვების წელი არის ${this.year}, ფერი - ${this.color}, ხოლო წონა შეადგენს ${this.weight}.`;
+//     }
+// };
+
+// console.log(car.getDescription());
+
+// console.log(Object.values(car));
+
+// car.brand = "BMW";
+// car.model = "M5";
+// car.year = 2024;
+// car.color = "Blue";
+// car.weight = "1800kg";
+
+// console.log(car);
+
+// car.mode = "Sport";
+
+// console.log(car);
+
+// delete car.mode;
+
+// console.log(car);
+
+// const myCarBrand = "BMW"; // შენი მანქანის ბრენდი
+
+// if (car.brand === myCarBrand) {
+//     console.log("იგივე მანქანის ბრენდი გვყოლია ძმობილო!");
+// } else {
+//     console.log("სხვადასხვა მანქანის ბრენდია, ჩემი მოუგებს! 😎");
+// }
